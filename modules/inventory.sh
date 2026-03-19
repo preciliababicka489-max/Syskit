dca1dbd0d7bed71e0199174c7a91d22d252bc253
preciliababicka <preciliababicka489@gmail.com>
2026-03-19 11:23:50 +0200

    Ajout des autres dossiers du projet

--- modules/inventory.sh
+++ modules/inventory.sh
@@ -1,19 +1,1 @@
-#/bin/bash
-echo "===== INVENTAIRE DU SYSTEME =====" 
-echo "Système d'exploitation :"
-grep PRETTY_NAME /etc/os-release
 
-echo "Version du noyau Linux"
-uname -r
-
-echo "Processeur :"
-lscpu | grep "Model name"
-
-echo "Mémoire RAM:"
-free -h
-
-echo "Espace disque :"
-df -h /
-
-echo "Adresse IP:"
-hostname  -I
