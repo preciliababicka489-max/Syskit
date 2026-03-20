fb3785817a5c73c1480b33883686cecee9a541ef
gabrielontsaga <gabrielontsaga462@gmail.com>
2026-03-20 16:51:59 +0100

    modification du backup.sh

--- modules/backup.sh
+++ modules/backup.sh
@@ -1,39 +1,20 @@
 #!/bin/bash
-
-#vérifier si un argument est fourni 
-if 
-[ -z "$1" ];
-then
-	echo "Usage :./syskit.sh backup <chemin/dossier>"
+#Vérifier les paramètres
+if [ "$1" != "backup" ] || [ -z "$2" ];
+then 
+	echo "Usage : ./syskit.sh backup <chemin/dossier>"
 	exit 1
-fi
-
-# Vérifier si le dossier existe
-if
-	[ !-d "$1" ];
-then
-	echo "Erreur: le dossier n'existe pas"
-	exit 1
-fi
-
-# Créer le dossier s'il n'existe pas
- mkdir -p ../backup
+fi 
+DIR="$2"
 
 # Récupérer le nom du dossier
-NAME=$(basename "$1")
+NAME=$(basename "$DIR")
 
 # Ajouter la date
-DATE=$(date + %Y-%m%-%d)
+DATE=$(date +"%Y-%m-%d")
 
 # Créer l'archive .tar
-tar -cf "../backup/${NAME}_$DATE.tar" "$1"
+tar -cf "../backup/${NAME}_$DATE.tar" -C "$(dirname "$DIR")" "$(basename "$DIR")"
 
-# Vérifier si la commande a réussi
-if 
-	[ $? -eq 0 ];
-then
-	echo "Sauvegarde réussie: ${NAME}_$DATE.tar"
-else
-	echo "Erreur lors de la sauvegarde"
-fi
+echo "Sauvegarde terminée"
 
