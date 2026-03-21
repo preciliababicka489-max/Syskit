371d8503d4e8f9f8502027528546ace92e51317b
preciliababicka <preciliababicka489@gmail.com>
2026-03-21 15:02:21 +0200

    Ajout des informations du rapport

--- modules/report.sh
+++ modules/report.sh
@@ -11,3 +11,30 @@ fi
 # Fichier de sortie dans le dossier reports
 REPORT_FILE="reports/report.txt"
 echo "RAPPORT SYSTEME" > "$REPORT_FILE"
+
+#Informations système
+echo "" >> "$REPORT_FILE"
+echo "Informations système" >> "$REPORT_FILE"
+./modules/inventory.sh >> "$REPORT_FILE"
+
+#Sauvegardes
+echo "" >> "$REPORT_FILE"
+echo "Sauvegardes" >> "$REPORT_FILE"
+
+if [ -d "backup" ];
+then
+	ls backup >> "$REPORT_FILE"
+else
+	echo " Aucune sauvegarde trouvée" >> "$REPORT_FILE"
+fi
+
+#Nettoyage
+echo "" >> "$REPORT_FILE"
+echo "Nettoyage" >> "$REPORT_FILE"
+
+if [ -f "logs/cleanup.log" ];
+then
+cat logs/cleanup.log >> "$REPORT_FILE"
+else
+	echo "Aucune information de nettoyage" >> "$REPORT_FILE"
+fi
